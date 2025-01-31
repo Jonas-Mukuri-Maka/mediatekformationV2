f341218b80462eaf6d28ab0afc590c8178b85f8c
zmuku <zmuku@LAPTOP-JAD21GOT>
2025-01-31 15:41:02 +0100

    Mission 1 Tache 1 : nettoyer le code

--- src/Controller/FormationsController.php
+++ src/Controller/FormationsController.php
@@ -27,6 +27,8 @@ class FormationsController extends AbstractController {
      */
     private $categorieRepository;
     
+    const FORMATIONS_TWIG_PATH = "pages/formations.html.twig";
+    
     function __construct(FormationRepository $formationRepository, CategorieRepository $categorieRepository) {
         $this->formationRepository = $formationRepository;
         $this->categorieRepository= $categorieRepository;
@@ -36,7 +38,7 @@ class FormationsController extends AbstractController {
     public function index(): Response{
         $formations = $this->formationRepository->findAll();
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::FORMATIONS_TWIG_PATH, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -46,7 +48,7 @@ class FormationsController extends AbstractController {
     public function sort($champ, $ordre, $table=""): Response{
         $formations = $this->formationRepository->findAllOrderBy($champ, $ordre, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::FORMATIONS_TWIG_PATH, [
             'formations' => $formations,
             'categories' => $categories
         ]);
@@ -57,7 +59,7 @@ class FormationsController extends AbstractController {
         $valeur = $request->get("recherche");
         $formations = $this->formationRepository->findByContainValue($champ, $valeur, $table);
         $categories = $this->categorieRepository->findAll();
-        return $this->render("pages/formations.html.twig", [
+        return $this->render(self::FORMATIONS_TWIG_PATH, [
             'formations' => $formations,
             'categories' => $categories,
             'valeur' => $valeur,
